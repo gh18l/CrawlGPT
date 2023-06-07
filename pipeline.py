183383baa86855a7d2ccc58f98f9fb801b5e5d1c
abcdcba3210123 <675157464@qq.com>
2023-06-07 22:07:26 +0800

    .

--- pipeline.py
+++ pipeline.py
@@ -1,10 +1,7 @@
 import os
 import json
 from typing import List
-os.environ["OPENAI_API_KEY"] = "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"
-import openai
-openai.organization = "org-2SJWplAgOe6VycEDktR4Ijkd"
-openai.api_key = "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"
+os.environ["OPENAI_API_KEY"] = ""
 os.environ["SERPER_API_KEY"] = "1ab03c5884c2bc2dd5f2df3ddb8d86684716cbd4"  # only used by testing
 from langchain import PromptTemplate, OpenAI, LLMChain
 from langchain.agents import load_tools, AgentExecutor
